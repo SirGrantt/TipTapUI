6e838158959edde4fe7fae4ba9816c8dfa1f3c01
SirGrantt <sirgrantt@gmail.com>
2018-06-27 20:59:32 -0500

    Added some styling to the home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {HomePageBanner, HomePageBannerWrapper, HomePageTitle } from '../styles/StyledComponents/HomePageBanner';
 //import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
-        <h1>TipTap for Virago</h1>
+      <HomePageBannerWrapper>
+      <HomePageBanner src="http://i1243.photobucket.com/albums/gg542/sirgrantt/TipCoffeeCup_zpsktpwrdhf.jpg" />
+      <HomePageBanner src="http://i1243.photobucket.com/albums/gg542/sirgrantt/fingerrtap_zpsl0etcrv3.jpg" />
+      <HomePageBanner src="http://i1243.photobucket.com/albums/gg542/sirgrantt/checkandphone_zpsqq371daz.jpg?t=1530064194" />
+      </HomePageBannerWrapper>
+        <HomePageTitle>TipTap for Virago</HomePageTitle>
         <br />
-        <h2>Service tips management with the tap of a finger.</h2>
+        <h2 style={{textAlign: 'center'}}>Service tips management with the tap of a finger.</h2>
     </div>
   );
 };
---
+++ src/styles/StyledComponents/HomePageBanner.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const HomePageBanner = styled.img`
+  width: 30%;
+  height: 250px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const HomePageBannerWrapper = styled.div`
+display: flex;
+`;
+
+export const HomePageTitle = styled.h1`
+text-align: center;
+`;
